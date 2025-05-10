4e693267bd721edc8833899d65f98942f1b416ae
43099151 <71186940+43099151@users.noreply.github.com>
2025-05-10 20:07:13 +0800

    Create init.sql

---
+++ scripts/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS maccms;
+CREATE USER IF NOT EXISTS 'maccms'@'localhost' IDENTIFIED BY 'maccms123';
+GRANT ALL PRIVILEGES ON maccms.* TO 'maccms'@'localhost';
+FLUSH PRIVILEGES;
